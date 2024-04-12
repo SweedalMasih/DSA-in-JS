162ce249c227c88fc451ad22586a433a0029ca54
SweedalMasih <sweedal.v.masih@gmail.com>
2024-04-12 23:34:21 +0530

    Binary Search Tree

--- Binary Search Tree/classCode.js
+++ Binary Search Tree/classCode.js
@@ -1,9 +1,3 @@
-class BinarySearchTree {
-  constructor() {
-    this.root = null;
-  }
-}
-
 class Node {
   constructor(value) {
     this.value = value;
@@ -11,3 +5,58 @@ class Node {
     this.right = null;
   }
 }
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return;
+    }
+    let current = this.root;
+    while (true) {
+      if (value < current.value) {
+        if (current.left == null) {
+          current.left = newNode;
+          return;
+        }
+        current = current.left;
+      } else {
+        if (current.right == null) {
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
+      }
+    }
+  }
+  bfs() {
+    const queue = [this.root];
+    const visited = [];
+
+    while (queue.length > 0) {
+      const node = queue.shift();
+      visited.push(node.value);
+
+      if (node.left !== null) {
+        queue.push(node.left);
+      }
+      if (node.right !== null) {
+        queue.push(node.right);
+      }
+    }
+
+    return visited;
+  }
+}
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(3);
+console.log(tree.bfs());
