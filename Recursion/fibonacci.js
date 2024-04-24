f1a3ef42beca0961e6206a9c9066ef0c9be96cab
SweedalMasih <sweedal.v.masih@gmail.com>
2024-04-24 23:50:55 +0530

    Recursion

--- Recursion/fibonacci.js
+++ Recursion/fibonacci.js
@@ -6,4 +6,12 @@ function fibonacci(n) {
   }
 }
 
-console.log(fibonacci(5));
+function fibonacci2(n) {
+  if (n === 1) return 0;
+
+  if (n === 2) return 1;
+
+  return fibonacci2(n - 1) + fibonacci2(n - 2);
+}
+
+console.log(fibonacci2(7));
