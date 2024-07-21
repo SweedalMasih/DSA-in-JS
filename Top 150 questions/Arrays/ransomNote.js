08da973882cee231a99209ccf03dbd0c056f9817
SweedalMasih <sweedal.v.masih@gmail.com>
2024-07-21 23:26:53 +0530

    Hashmap - Ransom Note

--- Top 150 questions/Arrays/ransomNote.js
+++ Top 150 questions/Arrays/ransomNote.js
@@ -1,5 +1,6 @@
 var canConstruct = function (ransomNote, magazine) {
   // this approach only checks for contiguous substring
+  /*
   const substrings = new Map();
   const length = magazine.length;
 
@@ -12,4 +13,26 @@ var canConstruct = function (ransomNote, magazine) {
   if (substrings.has(ransomNote)) return true;
 
   return false;
+  */
+
+  // correct approach
+
+  const charCount = new Map();
+
+  for (const char of magazine) {
+    if (charCount.has(char)) {
+      charCount.set(char, charCount.get(char) + 1);
+    } else {
+      charCount.set(char, 1);
+    }
+  }
+
+  for (const char of ransomNote) {
+    if (!charCount.has(char) || charCount.get(char) === 0) {
+      return false;
+    }
+    charCount.set(char, charCount.get(char) - 1);
+  }
+
+  return true;
 };
